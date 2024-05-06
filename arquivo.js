3214311e0046b7155c9e948b11a68f9fd87c347d
Jess <minhas.redes.sociais08@gmail.com>
2024-05-06 08:17:31 +0100

    divisao

--- arquivo.js
+++ arquivo.js
@@ -1,12 +1,15 @@
 function soma(num1, num2) {
-    return num1+num2;
+    return num1 + num2;
 }
 
 function subtracao(num1, num2) {
-    return num1-num2;
+    return num1 - num2;
 }
 
 function multiplicacao(num1, num2) {
-    return num1*num2;
+    return num1 * num2;
+}
+function divisao(num1, num2) {
+    return num1 / num2;
 }
 
